a67b830844eff6897d9ee7b4135ba83802b8516a
Vitsky <vitskyds@gmail.com>
2018-05-13 17:50:04 +0300

    refac: optimized css for tablet and desktop

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-
-gulp.task('default', function(){
-   console.log('hi!');
-});
-
-browserSync.init({server: './'});
-browserSync.stream();
